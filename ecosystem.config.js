2ba3f98badf4425f3f8326490c28dcfd86967856
Herbert Verdida <38489033+bertdida@users.noreply.github.com>
2024-09-07 09:47:18 +0800

    fix: pm2 script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'trackexpense.app',
-      script: 'npm ',
+      script: 'npm',
       args: 'start',
       instances: 1,
       autorestart: true,

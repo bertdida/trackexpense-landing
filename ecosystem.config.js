79287631a7318b0d8ee5aa410b354cea0cf2d771
Herbert Verdida <38489033+bertdida@users.noreply.github.com>
2024-09-07 17:24:05 +0800

    fix: pm2 script set cwd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,11 @@ module.exports = {
   apps: [
     {
       name: 'trackexpense.app',
-      script: 'node_modules/next/dist/bin/next',
+      cwd: '/var/www/trackexpense.app',
+      script: './node_modules/next/dist/bin/next',
       args: 'start',
       instances: 1,
+      watch: false,
       autorestart: true,
       env: {
         NODE_ENV: 'production',

a8b047d73d024dbdb6b07ff49bed7363103226a0
Herbert Verdida <38489033+bertdida@users.noreply.github.com>
2024-09-13 22:06:14 +0800

    feat: added custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+export default function Custom404() {
+  return (
+    <div className="grid h-screen content-center">
+      <div className="flex flex-col items-center justify-center space-y-4">
+        <h1 className="text-4xl font-bold leading-tight text-neutral-700 md:text-5xl">
+          404
+        </h1>
+        <p className="text-sm text-neutral-700">Sorry we couldn&apos;t find this page.</p>
+
+        <Link
+          href="/"
+          className="btn rounded-full bg-gradient-to-tr from-pink-500 to-yellow-500 px-5 text-base text-white"
+        >
+          Back to Homepage
+        </Link>
+      </div>
+    </div>
+  );
+}

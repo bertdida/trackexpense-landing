556b85229cf96fa56257603973462a322a2f3263
Herbert Verdida <38489033+bertdida@users.noreply.github.com>
2025-05-31 10:25:32 +0800

    fixes #4: adjust comming soon color

--- src/app/components/CommingSoon.tsx
+++ src/app/components/CommingSoon.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const CommingSoon: React.FC = () => {
   return (
-    <span className="flex h-[25px] w-fit items-center justify-center rounded-2xl bg-yellow-100 pl-2 pr-2 text-xs font-bold text-yellow-500">
+    <span className="flex h-[25px] w-fit items-center justify-center rounded-2xl bg-yellow-100 pl-2 pr-2 text-xs font-bold text-yellow-700">
       Coming soon
     </span>
   );
